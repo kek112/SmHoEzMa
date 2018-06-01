da998a96aece0cf657e9fc0bc685f12d8d1c4901
kek112 <kek112@users.noreply.github.com>
2018-06-01 21:00:35 +0200

    Added SensorAPI for temperature/humidity
    provided own api for generating values so they can be used in the QT App.
    
    Written in ASP.NET, i recommend installing Conveyor by Keyoti extentsion for VS 2017 because it allows an easy start. Otherwise IIS has to be configured.

---
+++ SensorAPI/SensorAPI/Controllers/Stuff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SensorAPI.Controllers
+{
+    public class Stuff
+    {
+        public int GetRandomNumber(int minimum, int maximum)
+        {
+            Random random = new Random();
+            return random.Next(minimum, maximum);
+        }
+    }
+}
---
+++ SensorAPI/SensorAPI/Models/Humidity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SensorAPI.Models
+{
+    public class Humidity
+    {
+        public int HumidPerCent{ get; set; }
+        public String ModelName { get; set; }
+    }
+}
